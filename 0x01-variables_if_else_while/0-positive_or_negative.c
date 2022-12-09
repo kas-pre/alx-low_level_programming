53343c5badde0dac9a5607a0394ea26ca0762542
kas-pre <kasasirakp40@gmail.com>
2022-12-09 15:50:33 -0800

    Fix lint errors, and compilation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,11 @@ int main(void)
 
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%d is negative\n". n);
+		printf("%d is negative\n", n);
 	} else if (n == 0)
 	{
 		printf("%d is zero\n", n);
