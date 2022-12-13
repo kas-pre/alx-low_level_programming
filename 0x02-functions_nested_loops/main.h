c911e13b216234bdf208eb38a4c04cecd1f8ddb0
kas-pre <kasasirakp40@gmail.com>
2022-12-13 04:44:15 -0800

    Print _putchar followed by new line

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: parameter to compare
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and error is set appropriately
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+#endif
