33131168c806c1cc2d39ed407aaa7e49061762fc
kas-pre <kasasirakp40@gmail.com>
2022-12-13 11:55:03 -0800

    Print the n timestable starting with 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -92,4 +92,11 @@ int add(int, int);
 
 void print_to_98(int n);
 
+/**
+ * print_times_table - print multiplication table up to n
+ * @n: integer argument
+ */
+
+void print_times_table(int n);
+
 #endif
