4b0d2614fe681247c9780b0318aab628299da9ba
kas-pre <kasasirakp40@gmail.com>
2022-12-19 14:10:42 -0800

    Prints string characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every single chr of str, start with first
+ * @str: chars to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
