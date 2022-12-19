cd3831a2ccec91b36a93e64f6b26e1a784d95e49
kas-pre <kasasirakp40@gmail.com>
2022-12-19 13:27:08 -0800

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of variable with pointer.
+ * @n: int pointer to variable to update
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
