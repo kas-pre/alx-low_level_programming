2c2061ac62bbaf33c2d6c6c01e1794a14e3314c4
kas-pre <kasasirakp40@gmail.com>
2022-12-21 07:28:57 -0800

    Encode string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes given str using rot13
+ * @str: string to encode
+ * Return: encoded string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,6 +19,8 @@ char *cap_string(char *);
 
 char *leet(char *);
 
+char *rot13(char *);
+
 
 
 #endif /* MAIN_H */
